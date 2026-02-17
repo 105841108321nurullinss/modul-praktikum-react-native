9c3d3d108b2e8194be6429805b9d4b9cff893b4b
fherli <105841107521@student.unismuh.ac.id>
2026-02-17 22:58:02 +0800

    Tutorial 08: Dasar-Dasar Navigasi

--- src/screens/index.js
+++ src/screens/index.js
@@ -6,6 +6,6 @@
  */
 
 // Export screen di sini
-// export { default as HomeScreen } from './HomeScreen';
-// export { default as DetailScreen } from './DetailScreen';
-// export { default as ProfileScreen } from './ProfileScreen';
+export { default as HomeScreen } from './HomeScreen';
+export { default as DetailScreen } from './DetailScreen';
+export { default as ProfileScreen } from './ProfileScreen';
