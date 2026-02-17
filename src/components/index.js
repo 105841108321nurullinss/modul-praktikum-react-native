14c9a332b106ca4fccc6a7e12b3182ed1384bbad
fherli <105841107521@student.unismuh.ac.id>
2026-02-17 22:39:51 +0800

    Tutorial 07: Input Pengguna

--- src/components/index.js
+++ src/components/index.js
@@ -13,3 +13,4 @@ export { default as ProfileCard } from './ProfileCard';
 export { default as StyledButton } from './StyledButton';
 export { default as CounterButton } from './CounterButton';
 export { default as TodoItem } from './TodoItem';
+export { default as FormInput } from './FormInput';
